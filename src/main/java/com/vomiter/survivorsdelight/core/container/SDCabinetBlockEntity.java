ba1cec4f16c291bcab5778f98ad0cc9cff9c5d73
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2025-10-12 14:19:03 +0800

    1.5.2 fix stove bug

--- src/main/java/com/vomiter/survivorsdelight/core/container/SDCabinetBlockEntity.java
+++ src/main/java/com/vomiter/survivorsdelight/core/container/SDCabinetBlockEntity.java
@@ -1,6 +1,5 @@
 package com.vomiter.survivorsdelight.core.container;
 
-import net.dries007.tfc.common.blockentities.TFCChestBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import vectorwing.farmersdelight.common.block.entity.CabinetBlockEntity;
