6e2b10f1a814da728f6c17fd29fbcbb42f0a5869
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-01-18 00:27:01 +0800

    add oven temp compat fix broken advancement

--- src/main/java/com/vomiter/survivorsdelight/mixin/heat/source/compat/firmalife/Oven_HSMixin.java
+++ src/main/java/com/vomiter/survivorsdelight/mixin/heat/source/compat/firmalife/Oven_HSMixin.java
@@ -1,11 +1,11 @@
 package com.vomiter.survivorsdelight.mixin.heat.source.compat.firmalife;
 
-//import com.eerussianguy.firmalife.common.blockentities.OvenBottomBlockEntity;
+import com.eerussianguy.firmalife.common.blockentities.OvenBottomBlockEntity;
 import com.vomiter.survivorsdelight.HeatSourceBlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 
-//@Mixin(value = OvenBottomBlockEntity.class, remap = false)
+@Mixin(value = OvenBottomBlockEntity.class, remap = false)
 public abstract class Oven_HSMixin implements HeatSourceBlockEntity {
     @Shadow
     public abstract float getTemperature();
