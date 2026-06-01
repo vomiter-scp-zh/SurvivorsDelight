4d7802600d07e516d99a4cbf6d7dd9a287ae5521
vomiter-scp-zh <97169118+vomiter-scp-zh@users.noreply.github.com>
2026-06-01 20:48:16 +0800

    fix pie fix crash with other feast blocks

--- src/main/java/com/vomiter/survivorsdelight/mixin/food/block/PieBlock_SliceMixin.java
+++ src/main/java/com/vomiter/survivorsdelight/mixin/food/block/PieBlock_SliceMixin.java
@@ -1,11 +1,15 @@
 package com.vomiter.survivorsdelight.mixin.food.block;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
+import com.vomiter.survivorsdelight.SurvivorsDelight;
 import com.vomiter.survivorsdelight.common.food.block.DecayingPieBlockEntity;
 import com.vomiter.survivorsdelight.compat.firmalife.FLCompatHelpers;
 import net.dries007.tfc.common.component.food.FoodCapability;
 import net.dries007.tfc.common.component.food.FoodTrait;
 import net.dries007.tfc.common.component.food.IFood;
+import net.dries007.tfc.common.player.IPlayerInfo;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Holder;
@@ -13,10 +17,12 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.food.FoodData;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.neoforged.fml.ModList;
@@ -31,7 +37,7 @@ import vectorwing.farmersdelight.common.block.PieBlock;
 import vectorwing.farmersdelight.common.utility.ItemUtils;
 
 @Mixin(value = PieBlock.class, remap = false)
-public abstract class PieBlock_SliceMixin{
+public abstract class PieBlock_SliceMixin implements EntityBlock {
 
     @Shadow public abstract ItemStack getPieSliceItem();
 
@@ -41,26 +47,36 @@ public abstract class PieBlock_SliceMixin{
         if (!(blockEntity instanceof DecayingPieBlockEntity decay)) return;
         Direction direction = player.getDirection().getOpposite();
         ItemStack slice = getPieSliceItem();
+        SurvivorsDelight.LOGGER.info("[SD] foodSlice = {}", FoodCapability.get(slice).getData().nutrients());
         sdtfc$applyFoodFromDecay(decay, slice);
+        SurvivorsDelight.LOGGER.info("[SD] foodSlice = {}", FoodCapability.get(slice).getData().nutrients());
+
         ItemUtils.spawnItemEntity(level, slice, (double)pos.getX() + (double)0.5F, (double)pos.getY() + 0.3, (double)pos.getZ() + (double)0.5F, (double)direction.getStepX() * 0.15, 0.05, (double)direction.getStepZ() * 0.15);
         level.playSound(null, pos, SoundEvents.WOOL_BREAK, SoundSource.PLAYERS, 0.8F, 0.8F);
         cir.setReturnValue(ItemInteractionResult.SUCCESS);
     }
 
-    @ModifyVariable(method = "consumeBite", at = @At(value = "STORE"), ordinal = 0)
-    private ItemStack sdtfc$applyDecayToSlice(
-            ItemStack value,
-            @Local(argsOnly = true) Level level,
-            @Local(argsOnly = true) BlockPos pos
+    @WrapOperation(method = "consumeBite", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/food/FoodData;eat(Lnet/minecraft/world/food/FoodProperties;)V", remap = true))
+    private void sdtfc$eat(
+            FoodData instance,
+            FoodProperties foodProperties,
+            Operation<Void> original,
+            @Local(argsOnly = true) Player player,
+            @Local(argsOnly = true, name = "arg1") Level level,
+            @Local(argsOnly = true, name = "arg2") BlockPos pos
     ){
+        original.call(instance, foodProperties);
         BlockEntity blockEntity = level.getBlockEntity(pos);
-        if(blockEntity instanceof DecayingPieBlockEntity decay) sdtfc$applyFoodFromDecay(decay, value);
-        return value;
+        if(blockEntity instanceof DecayingPieBlockEntity decay) {
+            var sliceStack = getPieSliceItem();
+            sdtfc$applyFoodFromDecay(decay, sliceStack);
+            IPlayerInfo.get(player).eat(sliceStack);
+        }
     }
 
     @ModifyVariable(method = "consumeBite", at = @At(value = "STORE"), name = "sliceFood")
     private FoodProperties sdtfc$modifyAddedEffects(
-            FoodProperties value,
+            FoodProperties sliceFood,
             @Local(argsOnly = true, name = "arg1") Level level,
             @Local(argsOnly = true, name = "arg2") BlockPos pos
     ){
@@ -68,13 +84,13 @@ public abstract class PieBlock_SliceMixin{
         if(blockEntity instanceof DecayingPieBlockEntity decay) {
             if(decay.isRotten()){
                 FoodProperties.Builder fakeFoodBuilder = new FoodProperties.Builder();
-                value.effects().forEach(pe -> {
+                sliceFood.effects().forEach(pe -> {
                     if(!pe.effect().getEffect().value().isBeneficial()) fakeFoodBuilder.effect(pe::effect, pe.probability());
                 });
                 return fakeFoodBuilder.build();
             }
         }
-        return value;
+        return sliceFood;
     }
 
 
@@ -82,11 +98,10 @@ public abstract class PieBlock_SliceMixin{
     private static void sdtfc$applyFoodFromDecay(DecayingPieBlockEntity decay, ItemStack slice) {
         ItemStack src    = decay.getStack();
         IFood srcFood    = FoodCapability.get(src);
-        IFood sliceFood  = FoodCapability.get(slice);
-        if (srcFood == null || sliceFood == null) return;
+        if (srcFood == null) return;
 
+        FoodCapability.setFoodForDynamicItemOnCreate(slice, srcFood.getData());
         FoodCapability.setCreationDate(slice, srcFood.getCreationDate());
-        FoodCapability.updateFoodFromPrevious(src, slice);
         if(ModList.get().isLoaded("firmalife")){
             for (Holder<FoodTrait> possibleShelvedFoodTrait : FLCompatHelpers.getPossibleShelvedFoodTraits()) {
                 FoodCapability.removeTrait(slice, possibleShelvedFoodTrait);
--- src/main/java/com/vomiter/survivorsdelight/registry/SDBlockEntityTypes.java
+++ src/main/java/com/vomiter/survivorsdelight/registry/SDBlockEntityTypes.java
@@ -4,12 +4,15 @@ import com.vomiter.survivorsdelight.SurvivorsDelight;
 import com.vomiter.survivorsdelight.common.container.SDCabinetBlockEntity;
 import com.vomiter.survivorsdelight.common.food.block.DecayingFeastBlockEntity;
 import com.vomiter.survivorsdelight.common.food.block.DecayingPieBlockEntity;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import vectorwing.farmersdelight.common.block.FeastBlock;
+import vectorwing.farmersdelight.common.block.PieBlock;
 import vectorwing.farmersdelight.common.registry.ModBlocks;
 
 public class SDBlockEntityTypes {
@@ -24,6 +27,20 @@ public class SDBlockEntityTypes {
                 .toArray(Block[]::new);
     }
 
+    private static Block[] feastBlock(){
+        return BuiltInRegistries.BLOCK
+                .stream()
+                .filter(b -> b instanceof FeastBlock)
+                .toArray(Block[]::new);
+    }
+
+    private static Block[] pieBlock(){
+        return BuiltInRegistries.BLOCK
+                .stream()
+                .filter(b -> b instanceof PieBlock)
+                .toArray(Block[]::new);
+    }
+
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<SDCabinetBlockEntity>> SD_CABINET =
             BLOCK_ENTITIES.register("cabinet",
                     () -> BlockEntityType.Builder.of(SDCabinetBlockEntity::new, cabinetBlocks()).build(null));
@@ -32,20 +49,14 @@ public class SDBlockEntityTypes {
             BLOCK_ENTITIES.register("feast_decaying",
                     () -> BlockEntityType.Builder.of(
                             DecayingFeastBlockEntity::new,
-                            ModBlocks.ROAST_CHICKEN_BLOCK.get(),
-                            ModBlocks.STUFFED_PUMPKIN_BLOCK.get(),
-                            ModBlocks.HONEY_GLAZED_HAM_BLOCK.get(),
-                            ModBlocks.SHEPHERDS_PIE_BLOCK.get(),
-                            ModBlocks.RICE_ROLL_MEDLEY_BLOCK.get()
+                            feastBlock()
                     ).build(null));
 
     public static final DeferredHolder<BlockEntityType<?>, BlockEntityType<DecayingPieBlockEntity>> PIE_DECAYING =
             BLOCK_ENTITIES.register("pie_decaying",
                     () -> BlockEntityType.Builder.of(
                             DecayingPieBlockEntity::new,
-                            ModBlocks.APPLE_PIE.get(),
-                            ModBlocks.CHOCOLATE_PIE.get(),
-                            ModBlocks.SWEET_BERRY_CHEESECAKE.get()
+                            pieBlock()
                     ).build(null));
 
     public static void register(IEventBus modEventBus) {
